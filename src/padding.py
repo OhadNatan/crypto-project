91bc640fbd131cfcc1e93ca21b8bc5c460158e57
Ohadnatan <onatan@augury.com>
2021-05-16 17:30:54 +0300

    unpadding

--- src/padding.py
+++ src/padding.py
@@ -3,3 +3,7 @@ def padding_text_by_block_size(text, block_size=16):
     amount_of_padding = block_size - len(text) % block_size
     text += "{"*amount_of_padding
     return text, amount_of_padding
+
+
+def unpadding_text_by_block_size(text, amount_of_padding):
+    return text[:-amount_of_padding]
