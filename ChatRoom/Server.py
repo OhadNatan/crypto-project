f11b849058cb6ac8560716d15407329e8bf5793d
Ohadnatan <onatan@augury.com>
2021-05-16 16:53:43 +0300

    chat room

---
+++ ChatRoom/Server.py
@@ -0,0 +1,57 @@
+import socket
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description='create sever by IP and PORT given')
+
+# Add the arguments
+parser.add_argument('-IP',
+                       type=str,
+                       help='the host IP',
+                       default="0.0.0.0")
+
+parser.add_argument('-PORT',
+                       type=int,
+                       help='the server PORT',
+                       default=2001)
+
+# Execute the parse_args() method
+args = parser.parse_args()
+
+localIP = args.IP
+
+localPort = args.PORT
+
+bufferSize = 1024
+
+msgFromServer = "Hello UDP Client"
+
+bytesToSend = str.encode(msgFromServer)
+
+# Create a datagram socket
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind to address and ip
+
+UDPServerSocket.bind((localIP, localPort))
+
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+
+while (True):
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+    message = bytesAddressPair[0]
+
+    address = bytesAddressPair[1]
+
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP = "Client IP Address:{}".format(address)
+
+    print(clientMsg)
+    print(clientIP)
+
+    # Sending a reply to client
+    UDPServerSocket.sendto(bytesToSend, address)
