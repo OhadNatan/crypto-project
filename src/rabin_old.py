b7b6fc6a930e0f342feb1c2186d0afddf393fae7
Ohadnatan <onatan@augury.com>
2021-06-11 17:36:24 +0300

    yes

--- src/rabin_old.py
+++ src/rabin_old.py
@@ -1,6 +1,5 @@
 import hashlib, sys
-
-nrabin = 0x1541942cc552a95c4832350ce99c2970f5b3ce9237a09c70c0e867d28039c05209b601105d3b3634cdaee4931809bc0c41d6165a0df16829a3a31202f56003239dd2c6e12297e94ef03e6aa61a147ea2b51c476dc45f5a2406b66d1ece2755c1f3d4144c0a42acc99b599d0643654a4cac392efbcf3db84d4233834afd1
+import random
 
 
 def gcd(a, b):
@@ -45,16 +44,17 @@ def h(x):
 # m: bytes
 def root(m, p, q):
     i = 0
+    nrabin = p*q
     while True:
         x = h(m) % nrabin
         sig = pow(p, q - 2, q) * p * pow(x, (q + 1) // 4, q)
         sig = (pow(q, p - 2, p) * q * pow(x, (p + 1) // 4, p) + sig) % (nrabin)
-        if (sig * sig) % nrabin == x:
+        if (sig * sig) % nrabin == x and i != 0:
             break
         m = m + bytes.fromhex("00")
         i = i + 1
     print("paddingnum: " + str(i))
-    return sig
+    return sig, i
 
 
 def writeNumber(number, fnam):
@@ -62,42 +62,49 @@ def writeNumber(number, fnam):
         f.write('%d' % number)
 
 
-def readNumber(fnam):
-    with open(fnam + '.txt', 'r') as f:
-        return int(f.read())
-
-
-def hF(m, paddingnum):
+def hF(m, paddingnum, nrabin):
     return h(m + bytes.fromhex("00") * paddingnum) % nrabin
 
 
-def sF(hexmsg):
-    p = readNumber("p")
-    q = readNumber("q")
+def sF(hexmsg, p, q):
     return root(bytes.fromhex(hexmsg), p, q)
 
 
-def vF(hexmsg, paddingnum, s):
-    return hF(bytes.fromhex(hexmsg), paddingnum) == (s * s) % nrabin
+def vF(hexmsg, paddingnum, s, nrabin):
+    return hF(bytes.fromhex(hexmsg), paddingnum, nrabin) == (s * s) % nrabin
 
-if __name__ == '__main__':
-    choice = 'V'
 
-    if choice == "V":
-        to_ver = '00112233445566778899aabbccddeeff'
-        paddingnum = '5'
-        digital = '0x10072ba7f1494016ad6193ea1862ffd2e570fbe1a765ec68641853db28ba44734a011f282b79a4321dee7bd9585f02a5cf8162f7ded88a98320577aec11cb55951779657df3dd65599813a4ffb93608a3b36b345cfe71dc7ac38e69402c2fc26edc988b68ba5865b126cf8fd2be2afd239ea05dc711771f658a07e69927'
-        print("result of verification: " + str(vF(to_ver, int(paddingnum), int(digital, 16))))
+def generate_keys_for_rabin():
+    arg = '{}{}'.format((random.randint(a=0, b=9)), random.randint(a=0, b=9))
+    p = nextPrime(h(bytes.fromhex(arg)) % (2 ** 501 + 1))
+    q = nextPrime(h(bytes.fromhex(arg + '00')) % (2 ** 501 + 1))
+    return p, q, p * q
 
-    if choice == "S":
-        msg = '00112233445566778899aabbccddeeff'
-        print((" digital signature:\n " + hex(sF(msg))))
+
+def sing_msg(msg, p, q):
+    sig, padding_num = sF(msg, p, q)
+    return hex(sig), padding_num
+
+
+def verify(to_ver, sig, padding_num, nrabin):
+    print("result of verification: " + str(vF(to_ver, int(padding_num), int(sig, 16), nrabin)))
+
+
+def main(choice=None, msg=None, p=None, q=None, n=None, padding_num=None, sig=None):
+
+    if choice == "V" and msg and sig and padding_num:
+        verify(msg, sig, padding_num, n)
+
+    if choice == "S" and p and q and msg:
+        return sing_msg(msg, p, q)
 
     if choice == "G":
-        arg = '01'
-        print(" generate primes ... ")
-        p = nextPrime(h(bytes.fromhex(arg)) % (2 ** 501 + 1))
-        q = nextPrime(h(bytes.fromhex(arg + '00')) % (2 ** 501 + 1))
-        writeNumber(p, 'p')
-        writeNumber(q, 'q')
-        print("nrabin = ", hex(p * q))
+        return generate_keys_for_rabin()
+
+
+msg_to_sig = '00112233445566778899aabbccddeeff'
+p, q, n = main('G')
+
+sig, pad_num = main('S', msg=msg_to_sig, p=p, q=q)
+
+main('V', sig=sig, msg=msg_to_sig, padding_num=pad_num, n=n)
